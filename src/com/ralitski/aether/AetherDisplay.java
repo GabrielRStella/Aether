20cb91fe703c9ba34ef9adf0568cfe16b354a551
Gabriel S <XyPhoGR@gmail.com>
2015-04-11 16:15:54 -0500

    fix error

--- src/com/ralitski/aether/AetherDisplay.java
+++ src/com/ralitski/aether/AetherDisplay.java
@@ -44,7 +44,7 @@ public class AetherDisplay extends GuiOwnerGL {
 		context.setup();
 		setTitle(context.getTitle());
 		Gui gui = new Gui(guiManager);
-		GuiGame game = new GuiGame(gui, display, context);
+		GuiGame game = new GuiGame(gui, this, context);
 		//TODO: add menu stuff
 		return game;
 	}
