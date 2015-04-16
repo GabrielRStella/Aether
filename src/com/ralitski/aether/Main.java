9dd01c92841d7a533d69a1409715f6713b00957d
Gabriel S <XyPhoGR@gmail.com>
2015-04-16 17:16:47 -0500

    todo

--- src/com/ralitski/aether/Main.java
+++ src/com/ralitski/aether/Main.java
@@ -4,14 +4,14 @@ package com.ralitski.aether;
  * TODO:
  * GameContext
  * modding/plugins (loader, hooks)
- * ViewBox
- * replace timer with timestep calculator
  * possibly add GameContext rendering? idk
- * allow player to switch between GameContexts and WorldRenderers
+ * allow player to switch between GameContexts before game starts
  * add support for random textures (ie, circle1.png circle2.png circle3.png)
  * change BoundingBox2d to support rotation (for screen and world stuff)
- * change input to support rotated movement
  * keep track of controller joystick as mouse movement in gui
+ * add objects that keep track of a few keyboard/controller states and return a vector() on update
+ * support for n player planets
+ * menus and options
  * 
  * @author ralitski
  *
