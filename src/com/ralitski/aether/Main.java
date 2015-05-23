b89d29ad4bb618c727296f7ab5ca782d90f3f3d1
Gabriel S <XyPhoGR@gmail.com>
2015-05-23 12:49:52 -0500

    todo

--- src/com/ralitski/aether/Main.java
+++ src/com/ralitski/aether/Main.java
@@ -4,7 +4,7 @@ package com.ralitski.aether;
  * TODO:
  * modding/plugins (loader, hooks)
  * allow player to switch between GameContexts before game starts
- * add support for random textures (ie, circle1.png circle2.png circle3.png)
+ * add support for random textures (ie, circle1.png circle2.png circle3.png) - or differently shaped planets
  * change BoundingBox2d to support rotation (for screen and world stuff)
  * keep track of controller joystick as mouse movement in gui
  * add objects that keep track of a few keyboard/controller states and return a vector() on update
