69b947f8d478dfbd72a56d48f96979f4c96be276
Gabriel S <XyPhoGR@gmail.com>
2015-05-30 14:54:21 -0500

    re-add main menu components

--- src/com/ralitski/aether/Main.java
+++ src/com/ralitski/aether/Main.java
@@ -3,18 +3,19 @@ package com.ralitski.aether;
 /**
  * TODO:
  * 
- * fix gui system RenderList bug
+ * get rid of old GLImage objects
+ * lag profiler
+ * fix lag on game start
  * 
  * modding/plugins (loader, hooks)
  * allow player to switch between GameContexts before game starts
  * add support for random textures (ie, circle1.png circle2.png circle3.png) - or differently shaped planets
- * change BoundingBox2d to support rotation (for screen and world stuff)
+ * fix BoundingBox2d collision detector
  * keep track of controller joystick as mouse movement in gui
  * menus and options
  * better input API
  * rotating world
  * add goal
- * menu text...?
  * 
  * @author ralitski
  *
