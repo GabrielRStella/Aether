a0f99b244230d3114bf763a93927f0df1d0b047f
Gabriel S <XyPhoGR@gmail.com>
2015-06-06 00:02:07 -0500

    move reference

--- src/com/ralitski/aether/mod/PluginData.java
+++ src/com/ralitski/aether/mod/PluginData.java
@@ -8,12 +8,4 @@ package com.ralitski.aether.mod;
  *
  */
 public interface PluginData {
-	
-	/*
-URLClassLoader child = new URLClassLoader (myJar.toURL(), this.getClass().getClassLoader());
-Class classToLoad = Class.forName ("com.MyClass", true, child);
-Method method = classToLoad.getDeclaredMethod ("myMethod");
-Object instance = classToLoad.newInstance ();
-Object result = method.invoke (instance);
-	 */
 }
--- src/com/ralitski/aether/mod/PluginLoader.java
+++ src/com/ralitski/aether/mod/PluginLoader.java
@@ -7,6 +7,18 @@ package com.ralitski.aether.mod;
  *
  */
 public class PluginLoader {
+
 	
+	/*
+URLClassLoader child = new URLClassLoader (myJar.toURL(), this.getClass().getClassLoader());
+Class classToLoad = Class.forName ("com.MyClass", true, child);
+Method method = classToLoad.getDeclaredMethod ("myMethod");
+Object instance = classToLoad.newInstance ();
+Object result = method.invoke (instance);
+	 */
+	
+	/*
+	 * note: Jar class loader must be kept with each plugin and trashed on refresh (so classes can be dynamically loaded; can't/shouldn't be loaded all at once)
+	 */
 	
 }
